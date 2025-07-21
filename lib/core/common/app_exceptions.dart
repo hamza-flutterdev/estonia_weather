ed31605a299150e3c69a5429f4dd0a651d0beb77
Hamza Shahid <hamzabutthb553.hb@gmail.com>
2025-07-21 14:31:27 +0500

    21/07/2025 - 2:30

---
+++ lib/core/common/app_exceptions.dart
@@ -0,0 +1,14 @@
+final String deniedPermission =
+    'Location services are disabled. Please enable location services.';
+final String timeoutException = 'Failed to get current location';
+final String successCityChange = 'Current location is now the main city';
+final String failToSave = 'Failed to save selected cities to storage';
+final String failedCityChange = 'Failed to add current location';
+final String failedApiCall = 'Failed to call API';
+final String noCityFound = 'No cities found matching';
+final String noCityInApi = 'City name not found in the response';
+final String firstLaunch = 'Error checking first launch';
+final String failedLoadingWeather = 'Failed to load weather data';
+final String noInternet = 'No internet at startup – retry dialog shown';
+final String failedLocation =
+    'Failed to detect your current location. Try again later.';
